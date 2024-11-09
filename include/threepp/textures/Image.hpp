f8b1028414f3d013a826b4284a39582ad06b3954
jusik <jusik1345@gmail.com>
2024-11-10 07:37:42 +0900

    Initial commit

---
+++ include/threepp/textures/Image.hpp
@@ -0,0 +1,49 @@
+
+#ifndef THREEPP_IMAGE_HPP
+#define THREEPP_IMAGE_HPP
+
+#include <utility>
+#include <variant>
+#include <vector>
+
+namespace threepp {
+
+    typedef std::variant<std::vector<unsigned char>, std::vector<float>> ImageData;
+
+    class Image {
+
+    public:
+        unsigned int width;
+        unsigned int height;
+        unsigned int depth;
+
+        Image(ImageData data, unsigned int width, unsigned int height, bool flipped = true)
+            : width(width), height(height), depth(0), flipped_(flipped), data_(std::move(data)){};
+
+        Image(ImageData data, unsigned int width, unsigned int height, unsigned int depth, bool flipped = true)
+            : width(width), height(height), depth(depth), flipped_(flipped), data_(std::move(data)){};
+
+        [[nodiscard]] bool flipped() const {
+
+            return flipped_;
+        }
+
+        void setData(ImageData data) {
+
+            data_ = std::move(data);
+        }
+
+        template<class T = unsigned char>
+        [[nodiscard]] std::vector<T>& data() {
+
+            return std::get<std::vector<T>>(data_);
+        }
+
+    private:
+        bool flipped_;
+        ImageData data_;
+    };
+
+}// namespace threepp
+
+#endif//THREEPP_IMAGE_HPP
